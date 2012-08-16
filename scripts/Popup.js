c1afcb9a4fd03f27eab9b09f707b6a0accbf3fde
rechie1985 <lsl1276@gmail.com>
2012-08-16 18:55:19 +0800

    删除bookstrap.css引用

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -120,53 +120,7 @@ function WIZPlugin() {
 
 	this.getCookies = getCookies;
 	this.autoLogin = autoLogin;
-}
-
-window.onload = function() {
-	initPopupPage();
-	var plugin = new WIZPlugin();
-	var url = mainUrl;
-	plugin.getCookies(url, "wiz-clip-auth", showByCookies);
-	// We need this so the extension knows when this window's been dismissed (by virtue of this conenction dying).
-	chrome.extension.connect({
-		name : "popupClosed"
-	});
-
-	function showByCookies(cookies) {
-
-		if (cookies) {
-			plugin.autoLogin(cookies);
-		} else {
-			$("#waiting").hide();
-			//cookie中未保存或已过期
-			$("#wiz_login").show();
-			$("#wiz_clip_detail").hide();
-		}
-	}
-
-	function initPopupPage() {
-		$("#waiting-label").html(chrome.i18n.getMessage("popup_wating"));
-
-		//login page
-		$("#user_id_tip").html(chrome.i18n.getMessage("user_id_tip"));
-		$("#password_tip").html(chrome.i18n.getMessage("password_tip"));
-		$("#keep_password_tip").html(chrome.i18n.getMessage("keep_password_tip"));
-		$("#login_button").html("&nbsp;" + chrome.i18n.getMessage("login_msg") + "&nbsp;");
-
-		//note info page
-		$("#note_title_tip").html(chrome.i18n.getMessage("note_title_tip"));
-		$("#category_tip").html(chrome.i18n.getMessage("category_tip"));
-		$("#tag_tip").html(chrome.i18n.getMessage("tag_tip"));
-		$("#tag_input").html(chrome.i18n.getMessage("tag_input"));
-		$("#note_submit").html("&nbsp;" + chrome.i18n.getMessage("note_submit") + "&nbsp;");
-		$("#article_save").html(chrome.i18n.getMessage("article_save"));
-		$("#fullpage_save").html(chrome.i18n.getMessage("fullpage_save"));
-		$("#select_save").html(chrome.i18n.getMessage("select_save"));
-		$("#url_save").html(chrome.i18n.getMessage("url_save"));
-	}
-
-}
-function ClipPageControl() {
+	
 	$("#note_submit").live("click", noteSubmit);
 
 	/**
@@ -235,6 +189,52 @@ function ClipPageControl() {
 		port.postMessage(opCmd);
 		//save and close
 		window.close();
+	}
+
+}
+
+
+window.onload = function() {
+	initPopupPage();
+	var plugin = new WIZPlugin();
+	var url = mainUrl;
+	plugin.getCookies(url, "wiz-clip-auth", showByCookies);
+	// We need this so the extension knows when this window's been dismissed (by virtue of this conenction dying).
+	chrome.extension.connect({
+		name : "popupClosed"
+	});
+
+	function showByCookies(cookies) {
+
+		if (cookies) {
+			plugin.autoLogin(cookies);
+		} else {
+			$("#waiting").hide();
+			//cookie中未保存或已过期
+			$("#wiz_login").show();
+			$("#wiz_clip_detail").hide();
+		}
+	}
+
+	function initPopupPage() {
+		$("#waiting-label").html(chrome.i18n.getMessage("popup_wating"));
+
+		//login page
+		$("#user_id_tip").html(chrome.i18n.getMessage("user_id_tip"));
+		$("#password_tip").html(chrome.i18n.getMessage("password_tip"));
+		$("#keep_password_tip").html(chrome.i18n.getMessage("keep_password_tip"));
+		$("#login_button").html("&nbsp;" + chrome.i18n.getMessage("login_msg") + "&nbsp;");
+
+		//note info page
+		$("#note_title_tip").html(chrome.i18n.getMessage("note_title_tip"));
+		$("#category_tip").html(chrome.i18n.getMessage("category_tip"));
+		$("#tag_tip").html(chrome.i18n.getMessage("tag_tip"));
+		$("#tag_input").html(chrome.i18n.getMessage("tag_input"));
+		$("#note_submit").html("&nbsp;" + chrome.i18n.getMessage("note_submit") + "&nbsp;");
+		$("#article_save").html(chrome.i18n.getMessage("article_save"));
+		$("#fullpage_save").html(chrome.i18n.getMessage("fullpage_save"));
+		$("#select_save").html(chrome.i18n.getMessage("select_save"));
+		$("#url_save").html(chrome.i18n.getMessage("url_save"));
 	}
 
 }
